bdea29f2ecd791aba2071c297b89bb9c6537a801
Victor Zapata <Victor_Zapata@epam.com>
2025-10-17 15:48:38 -0500

    Add missing classes

---
+++ src/main/java/co/ateunti/brolo/target/serializer/StatusInfoSerializer.java
@@ -0,0 +1,35 @@
+package co.ateunti.brolo.target.serializer;
+
+import co.ateunti.brolo.target.model.StatusInfo;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.apache.kafka.common.errors.SerializationException;
+import org.apache.kafka.common.serialization.Serializer;
+
+import java.util.Map;
+
+public class StatusInfoSerializer implements Serializer<StatusInfo> {
+    private final ObjectMapper objectMapper = new ObjectMapper();
+
+    @Override
+    public void configure(Map<String, ?> configs, boolean isKey) {
+        // No configuration needed
+    }
+
+    @Override
+    public byte[] serialize(String topic, StatusInfo data) {
+        try {
+            if (data == null) {
+                System.out.println("Null received at serializing");
+                return null;
+            }
+            return objectMapper.writeValueAsBytes(data);
+        } catch (Exception e) {
+            throw new SerializationException("Error when serializing CustomObject to byte[]", e);
+        }
+    }
+
+    @Override
+    public void close() {
+        // No resources to close
+    }
+}
